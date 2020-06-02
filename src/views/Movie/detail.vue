397bd329075d0af0f02cd8a942d7c88ef346895d
15156047553 <2742645612@qq.com>
2020-06-02 11:05:39 +0800

    第五次提交

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -6,7 +6,7 @@
   <Loading v-if="isLoading" />
   <div v-else id="content" class="contentDetail">
     <div class="detail_list">
-      <div class="detail_list_bg"></div>
+      <div class="detail_list_bg" :style="{'background-image': `url(${detailMovie.img.replace(/w\.h/, '216.300')})`}"></div>
       <div class="detail_list_filter"></div>
       <div class="detail_list_content">
         <div class="detail_list_img">
@@ -29,7 +29,7 @@
       <ul class="swiper-wrapper">
         <li class="swiper-slide" v-for="(item, index) in detailMovie.photos" :key="index">
           <div>
-            <img :src="item | setWH('70.70')" alt="">
+            <img :src="item | setWH('')" alt="">
           </div>
         </li>
       </ul>
@@ -120,7 +120,7 @@ export default {
 .detail_list .detail_list_bg {
     width:100%;
     height:100%;
-    background: url(/images/movie_1.jpg) 0 40%;
+    background: 0 40%;
     filter: blur(20px);
     background-size:cover;
     position: absolute;
